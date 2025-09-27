bd6eabe8b988831229bda03eb25fc8af865f700c
Jfranciglez <jenniglez46@gmail.com>
2025-09-27 13:42:22 +0200

    creación de index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relación II PHP</title>
+    <style>
+    html{
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center; 
+        align-items: center;
+        background-color: #ebedf0ff;
+
+        }
+    #contenedor{
+        text-align: center;
+        border-radius: 20px;
+        background-color: #eef0bd ;
+        width: 800px;
+        height: 300px;
+        
+    }
+    #header{
+        background-color: rgb(235, 237, 107);
+        color: black;
+        border-radius: 20px;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+       
+    }
+    a:hover{
+        color: white;
+    }
+
+</style>
+</head>
+
+<body>
+    <div id="contenedor">
+        <div id="header">
+        <h2>Ejercicios-PHP-02</h2>
+        <h3>Certificaciones de Profesionalidad DAW</h3>
+        </div>
+        <a href="ejerc-01">>>Pedido online restaurante</a><br><br>
+        <a href="ejerc-02">>>Juego Apuesta y Gana</a>
+        
+    </div>
+</body>
+</html>
